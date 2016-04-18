a6a4d8ff916d5ded0c68ea1b0d74132af8b501b1
aileen santos <asantos3026@gmail.com>
2016-04-18 14:49:23 -0700

    completed 2nd half of hello.js exercise

--- src/hello.js
+++ src/hello.js
@@ -19,3 +19,20 @@ var Goodbye = React.createClass({
       );
   }
 }); 
+
+ReactDOM.render(<Goodbye/>, document.getElementById('goodbye'));
+
+var comments = [{author: "Aileen", text: "Hello Aileen"}, {author: "Lee", text: "Hello Lee"}];
+
+var Comment = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h1>{ this.props.author }</h1>
+        <p>{ this.props.text }</p>
+      </div> 
+    );
+  }
+});
+
+ReactDOM.render(<Comment author={comments[0].author} text={comments[0].text}/>, document.getElementById('comment'));
