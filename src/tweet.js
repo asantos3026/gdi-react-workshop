c8536fb8748fe9f2a51c33b5cd2deedce031af6d
aileen santos <asantos3026@gmail.com>
2016-04-18 21:38:19 -0700

    completed step 10 of app, Yay

--- src/tweet.js
+++ src/tweet.js
@@ -12,15 +12,15 @@ var Twitter = React.createClass({
       }.bind(this)
     );
   },
-  // handleTweetSubmit: function (author, text) {
-  //   var tweet = { author: author, text: text };
-  //
-  //   // POST to add tweet to database
-  //   $.post(this.props.url, tweet, function (data) {
-  //       // Set state in step 10 of the exercise!
-  //     }.bind(this)
-  //   );
-  // },
+  handleTweetSubmit: function (author, text) {
+    var tweet = { author: author, text: text };
+
+    // POST to add tweet to database
+    $.post(this.props.url, tweet, function (data) {
+        this.setState({ data: data });
+      }.bind(this)
+    );
+  },
   componentDidMount: function () {
     // Set this.state.data to most recent set of tweets from database
     this.loadTweetsFromServer();
@@ -29,7 +29,7 @@ var Twitter = React.createClass({
     return (
       <div className="twitter">
         <h1>Tweets</h1>
-        <TweetForm />
+        <TweetForm onTweetSubmit={ this.handleTweetSubmit } />
         <TweetList data={ this.state.data } />
       </div>
     );
@@ -40,13 +40,28 @@ var TweetForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
 
-    alert('You submitted the form!');
+    // Get new author and text from the input fields
+    var author = this.refs.author.value;
+    var text = this.refs.text.value;
+
+    // Do nothing if either input field is blank
+    if (!text || !author) {
+      return;
+    }
+
+    // Send new author and text up one level to Twitter component
+    // so updated tweets can be passed down again into TweetList component
+    this.props.onTweetSubmit(author, text);
+
+    // Set input fields back to empty
+    this.refs.author.value = '';
+    this.refs.text.value = '';
   },
   render: function () {
     return (
       <form className="tweetForm" onSubmit={ this.handleSubmit }>
-        <input type="text" placeholder="Author Name" />
-        <input type="text" placeholder="Tweet" />
+        <input type="text" placeholder="Author Name" ref="author" />
+        <input type="text" placeholder="Tweet" ref="text" />
         <button type="submit" className="btn btn-info">Tweet</button>
       </form>
     );
